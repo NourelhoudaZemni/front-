05f38b6ec26eb763a378ffd45e9bef9f0151a082
NourelhoudaZemni <79015609+NourelhoudaZemni@users.noreply.github.com>
2022-05-10 00:23:08 +0100

    kj

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/css/bootstrap.min.css";
+import ReactDOM from "react-dom"; 
 import "./assets/css/flaticon.min.css";
 import "./assets/css/boxicon.min.css";
 import "./index.css";
